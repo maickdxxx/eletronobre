684c17a8a996ddbe3d8026212e2a8e034fd8f007
maickdxxx <michaeldouglas7991@gmail.com>
2026-08-12 19:08:45 -0300

    feat: activate Coruja compatibility enhancements

--- src/main.jsx
+++ src/main.jsx
@@ -3,5 +3,6 @@ import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./styles.css";
 import "./coruja-template/editor-dom.js";
+import "./coruja-template/compat-content.js";
 
 createRoot(document.getElementById("root")).render(<React.StrictMode><App /></React.StrictMode>);
